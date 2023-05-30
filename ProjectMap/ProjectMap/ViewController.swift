3a9ca9312a03c900217b3ee6787b5f3d5379c26e
rampafan05 <110806636+rampafan05@users.noreply.github.com>
2023-05-30 16:30:06 +0300

    first commit

---
+++ ProjectMap/ProjectMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectMap
+//
+//  Created by macbook on 30.05.2023.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
